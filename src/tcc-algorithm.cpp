49b5c1db949bf487a8fb7387ff9babac8a2677d5
DESKTOP-I6GK69U\Usuario <gustavo_chermout@hotmail.com>
2019-04-10 20:39:10 -0300

    Estruturas de dados

--- src/tcc-algorithm.cpp
+++ src/tcc-algorithm.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
+struct Point { 
+    double x; 
+    double y;
+    Point() {} 
+    Point(double _x, double _y) { 
+        x = _x; 
+        y = _y; 
+    } 
+}; 
+
+struct Rectangle {
+    int left, right, top, bottom;
+    Rectangle(){}
+    Rectangle(int _left, int _right, int _top, int _bottom){
+        left = _left;
+        right = _right;
+        top = _top;
+        bottom = _bottom;
+    }
+};
+
+struct Node {
+    Node *parent, *child[8];
+    vector<Point> points;
+    Rectangle rectangle;
+    Point start, end;
+    double cost; //non-leaf is (cost of tsp - euclidean distance between start and end point of each leaf)
+    Node(){}
+    Node(Node *_parent, Rectangle _rectangle){
+        parent = _parent;
+
+        for (int i = 0; i < 8; i++)
+            child[i] = NULL;
+        
+        rectangle = _rectangle;
+        points.clear();
+        cost = 0.000;        
+    }
+    bool isLeaf(){
+        return (child[0] == NULL);
+    }
+};
+
 int main(){
   
   return 0;   
